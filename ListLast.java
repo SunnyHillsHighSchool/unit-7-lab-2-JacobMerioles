8380db2ebe2366448b64956b2c3b818534d67235
Jacob Merioles <600021653@fjuhsd.org>
2021-02-01 23:15:28 +0000

    finished Unit 7 Lab 2

--- ListLast.java
+++ ListLast.java
@@ -1,15 +1,23 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Jacob Merioles
+//Date - 2/1/2021
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class ListLast
 {
-	public static boolean go( List<Integer> ray)
+	public static boolean go(List<Integer> ray)
 	{
-		return true;
+    if (ray.size() == 0 || ray.size() == 1){
+      return false; }
+    int lastNum = ray.get(ray.size()-1);
+    for (int i = 0; i <= ray.size()-2; i++)
+    {
+      if (ray.get(i) == lastNum){
+        return true; }
+    }
+		return false;
 	}
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Jacob Merioles
+//Date - 2/1/2021
 
 //add imports as needed
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class Main
 {
  public static void main( String args[] )
  {
+   ListLast s = new ListLast();
+   System.out.println( s.go( Arrays.asList(0,5876,5876) ) );
+   /*ArrayList<Integer> arr = new ArrayList<Integer>();
+   arr.add(5);
+   arr.add(6);
+   arr.add(7);
+   arr.add(10);
+   arr.add(9);
+   arr.add(10);
+   System.out.println(go(arr));*/
  }
 }
